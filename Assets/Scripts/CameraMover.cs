5a737dde4a2dd57706c1b17d06627e2751143944
OlafGomez <olaf.gomez@bigtech.services>
2023-12-16 19:12:24 +0100

    escenas travelling

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,35 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CameraMover : MonoBehaviour
+{
+    [SerializeField] 
+    private Camera cameraToMove;
+
+    [SerializeField] 
+    private Transform finalPoint;
+
+    [SerializeField]
+    private CharacterMovement characterMovement;
+    
+    [SerializeField] 
+    private float movementTime;
+    
+
+    [SerializeField] 
+    private UnityEvent onSceneChanged;
+
+    public void DoMoveCamera()
+    {
+        var finalPointNoZ = new Vector3(finalPoint.position.x, finalPoint.position.y, cameraToMove.transform.position.z);
+        cameraToMove.transform.DOMove(finalPointNoZ, movementTime).OnComplete(onSceneChanged.Invoke).SetDelay(characterMovement.MovementTime + .1f);
+    }
+
+    // private void OnTriggerExit2D(Collider2D other)
+    // {
+    //     var finalPointNoZ = new Vector3(finalPoint.position.x, finalPoint.position.y, cameraToMove.transform.position.z);
+    //     cameraToMove.transform.DOMove(finalPointNoZ, movementTime).OnComplete(onSceneChanged.Invoke);
+    // }
+
+}
